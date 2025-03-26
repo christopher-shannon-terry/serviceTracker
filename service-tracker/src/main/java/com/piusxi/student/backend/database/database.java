f53b3747e4dc4fd425bc0765c9d2e5612c36b594
alan-d-mitchell <alan.mitchell@piusxi.org>
2025-03-26 07:07:47 -0500

    BLEH BLOOP BLAH BLOOPITY BLEH BLAH BLAH BIBBITY BOPPITY BOOP

--- service-tracker/src/main/java/com/piusxi/student/backend/database/database.java
+++ service-tracker/src/main/java/com/piusxi/student/backend/database/database.java
@@ -1,4 +1,4 @@
-package com.piusxi;
+package com.piusxi.student.backend.database;
 
 public class database {
     public static void main(String[] args) {
---
+++ service-tracker/src/main/java/com/piusxi/student/frontend/verifyEmailPage.java
@@ -0,0 +1,24 @@
+package com.piusxi.student.frontend;
+
+import javax.swing.JFrame;
+
+public class verifyEmailPage extends JFrame {
+
+    public verifyEmailPage() {
+        setTitle("Email Verification");
+        setSize(800, 600);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        verify();
+    }
+
+    public void verify() {
+        
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
