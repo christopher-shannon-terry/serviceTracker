151ceae19bfc89923963bb2d9639b588d642ed70
alan-d-mitchell <alan.mitchell@piusxi.org>
2025-03-31 11:52:28 -0500

    ok

---
+++ service-tracker/src/main/java/com/piusxi/admin/backend/adminToken.java
@@ -0,0 +1,5 @@
+package com.piusxi.admin.backend;
+
+public class adminToken {
+    
+}
--- service-tracker/src/test/java/com/piusxi/databaseConnectionTest.java
+++ service-tracker/src/test/java/com/piusxi/databaseConnectionTest.java
@@ -1,5 +1,7 @@
 package com.piusxi;
 
 public class databaseConnectionTest {
-    
+    private static final String DB_URL = "jdbc:mariadb://localhost:3306/testdb";
+    private static final String USER = "alanmitchell";
+    private static final String PASSWORD = "922925";
 }
