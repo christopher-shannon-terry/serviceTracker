b9ed38409030295a09e9b6ca6b552bee1b8c694f
alan-d-mitchell <alan.mitchell@piusxi.org>
2025-04-07 08:17:54 -0500

    ok

--- service-tracker/src/main/java/com/piusxi/admin/database/adminDatabase.java
+++ service-tracker/src/main/java/com/piusxi/admin/database/adminDatabase.java
@@ -12,15 +12,15 @@ public class adminDatabase {
     public static final String USER = "alanmitchell";
     public static final String PASSWORD = "922925";
 
-    /* public static final String DB_URL = "jdbc:mariadb://localhost:3306/service_info";
+    /* public static final String DB_URL = "jdbc:mariadb://localhost:3306/admin_database";
     public static final String USER = "joshuachristian";
     public static final String PASSWORD = "123456"; */
 
-    /* public static final String DB_URL = "jdbc:mariadb://localhost:3306/service_info";
+    /* public static final String DB_URL = "jdbc:mariadb://localhost:3306/admin_database";
     public static final String USER = "ethancobb";
     public static final String PASSWORD = "123456"; */
 
-    /* public static final String DB_URL = "jdbc:mariadb://localhost:3306/service_info";
+    /* public static final String DB_URL = "jdbc:mariadb://localhost:3306/admin_database";
     public static final String USER = "shannonterry";
     public static final String PASSWORD = "123456"; */
 
