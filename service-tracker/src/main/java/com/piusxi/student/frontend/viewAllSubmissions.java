e52bb9c83e8c73e03a7776ecfd57ae0c6ea8ffbd
Alan Mitchell <alan.mitchell@piusxi.org>
2025-05-05 10:52:59 -0500

    Added file to view all submissions

---
+++ service-tracker/src/main/java/com/piusxi/student/frontend/viewAllSubmissions.java
@@ -0,0 +1,9 @@
+package com.piusxi.student.frontend;
+
+/**
+ * This class will show all the student's service submissions so far, with the ability to click on them to view the filled out form in its entirety
+ * Form im referencing is the serviceReportingForm.java file
+ */
+public class viewAllSubmissions {
+    
+}
